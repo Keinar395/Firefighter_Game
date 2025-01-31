0abe354cdf189e51fca06c44cf69b81f039ced97
Keinar395 <162704682+Keinar395@users.noreply.github.com>
2025-01-31 22:56:39 +0300

    yaptım ama bişiler eksik

---
+++ Assets/Secme/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int enmyDieTime = 3;
+    int currentHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        // sonradan hurt anim ve ses eklenecek buraya
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("düþman öldü");
+        //ölüm animasyonu ve sesi buraya eklenecek 
+        Destroy(gameObject, enmyDieTime);
+    }
+}
---
+++ Assets/Secme/MeleeCombat.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCombat : MonoBehaviour
+{
+    private Animator animator;
+    private bool hasDrawn = false;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleSwordDraw();
+        HandleAttack();
+    }
+
+
+    private void HandleSwordDraw()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !hasDrawn)
+        {
+            Debug.Log("F tuþuna basýldý, kýlýç çekiliyor...");
+            animator.SetBool("HasDrawn", true);
+            hasDrawn = true;
+        }
+    }
+
+
+    private void HandleAttack()
+    {
+        if (hasDrawn && Input.GetKeyDown(KeyCode.K))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        // Placeholder for attack logic (e.g., detecting and damaging enemies).
+        Debug.Log("Performed attack!");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers); ;
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
